a423685e8903c95b60d57ace44f9944735692e91
Mark Kunitski <markkmpya@gmail.com>
2023-09-25 07:15:41 +0200

    25.09 design changes

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,25 @@ var typed = new Typed("#typed", {
   loop: true,
   smartBackspace: true,
 });
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  var body = document.querySelector("body");
+
+  // Get the header height
+  var headerHeight = header.offsetHeight;
+
+  // Get the scroll position
+  var scrollY = window.scrollY || window.pageYOffset;
+
+  // Check if the scroll position is greater than the header height
+  if (scrollY > headerHeight) {
+    // Set the body background color to zinc-800
+    header.style.borderBottom = "1px solid #333339";
+
+    header.style.backgroundColor = "#18181b"; // Use your desired color code here
+  } else {
+    // Set the body background color to zinc-900
+    header.style.borderBottom = "1px solid transparent";
+    header.style.backgroundColor = "#27272a"; // Use your desired color code here
+  }
+});
