514fb85f167da5c5719113ded23fedfd23901b5c
Mark Kunitski <markkmpya@gmail.com>
2023-09-26 18:16:20 +0200

    26.09 RWD

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        DEFAULT: ".9rem",
-        sm: "1.5rem",
+        DEFAULT: "2rem",
+        sm: "2rem",
         lg: "2rem",
         xl: "3rem",
         "2xl": "4rem",
