ee8b75905f14d786eb781ab9c4af6bfc4dd6cfc3
markkunitski <markkmpya@gmail.com>
2023-10-06 22:24:58 +0200

    06.10 multilang

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,57 @@
+// LANG
+const english = document.querySelector('link[hreflang="en"]');
+const estonian = document.querySelector('link[hreflang="et"]');
+const russian = document.querySelector('link[hreflang="ru"]');
+// typing text
+let translated = [
+  "Website loading too slowly",
+  "Security breaches and vulnerabilities",
+  "App crashes on specific devices",
+  "Scalability for increased user traffic",
+  "Data loss and recovery strategies",
+  "Cart abandonment and checkout optimization",
+  "On-premises to cloud migration",
+  "Project deadline challenges",
+  "User experience improvement needed",
+];
+function stringsLang() {
+  if (estonian) {
+    estonian.href = `${window.location.origin}/et/`;
+    translated = [
+      "Veebisait laadib liiga aeglaselt",
+      "Turvalisuse rikkumised ja haavatavused",
+      "Rakenduse kokkujooksed konkreetsetel seadmetel",
+      "Skaleeritavus suurenenud kasutajaliikluse jaoks",
+      "Andmete kaotamise ja taastamise strateegiad",
+      "Korvi hülgamine ja kassasse optimeerimine",
+      "Kohalikult pilve migreerumine",
+      "Projekti tähtajaga seotud väljakutsed",
+      "Kasutajakogemuse parandamine vajalik",
+    ];
+    return;
+  }
+  if (russian) {
+    russian.href = `${window.location.origin}/ru/`;
+    translated = [
+      "Веб-сайт загружается слишком медленно",
+      "Нарушения безопасности и уязвимости",
+      "Падение приложения на определенных устройствах",
+      "Масштабируемость для увеличения трафика",
+      "Потеря данных и стратегии восстановления",
+      "Отказ от корзины и оптимизация оформления заказа",
+      "Миграция с локальных серверов в облако",
+      "Проблемы с сроками проекта",
+      "Требуется улучшение пользовательского опыта",
+    ];
+    return;
+  }
+  english.href = window.location.origin;
+}
+stringsLang();
 var typed = new Typed("#typed", {
-  strings: [
-    "Website loading too slowly",
-    "Security breaches and vulnerabilities",
-    "App crashes on specific devices",
-    "Scalability for increased user traffic",
-    "Data loss and recovery strategies",
-    "Cart abandonment and checkout optimization",
-    "On-premises to cloud migration",
-    "Project deadline challenges",
-    "User experience improvement needed",
-  ],
-  typeSpeed: 35,
-  backSpeed: 20,
+  strings: translated,
+  typeSpeed: 15,
+  backSpeed: 12,
   loop: true,
   smartBackspace: true,
 });
@@ -23,9 +63,9 @@ window.addEventListener("scroll", function () {
 
   if (scrollY > headerHeight) {
     if (this.document.documentElement.classList.contains("dark")) {
-      header.style.backgroundColor = "#18181b";
+      header.style.backgroundColor = "rgba(24, 24, 27, .8)";
     } else {
-      header.style.backgroundColor = "#dbdbdb";
+      header.style.backgroundColor = "rgb(219, 219, 219, .5)";
     }
     header.style.borderBottom = "1px solid #333339";
   } else {
@@ -70,8 +110,24 @@ if (
   (!("theme" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
-  themeToggle.checked = true
+  themeToggle.checked = true;
   document.documentElement.classList.add("dark");
 } else {
   document.documentElement.classList.remove("dark");
 }
+// SERVICES
+function handleExpand(event) {
+  let target = event.target;
+  if (target.classList.contains("EXPANDER")) {
+    target.textContent =
+      target.textContent.trim() === "Show the rest >"
+        ? "Hide >"
+        : "Show the rest >";
+    const parent = document.getElementById(event.target.dataset.parent);
+    const hidden = parent.querySelector(".whenHidden");
+    const shown = parent.querySelector(".whenShown");
+    hidden.classList.toggle("hidden");
+    shown.classList.toggle("hidden");
+  }
+}
+services.addEventListener("click", (e) => handleExpand(e));
